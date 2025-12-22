31df6c29a68d10bd8b128034fa7cc54ab3f8d986
Noah Wissler <noah.wissler@gmail.com>
2025-12-22 21:05:05 +0100

    made ui widgets resizing work

--- darts-counter/darts-counter.cpp
+++ darts-counter/darts-counter.cpp
@@ -1,18 +1,24 @@
 #include "darts-counter.h"
 #include <vector>
 #include <cstdint>
-struct player{
+#include <cmath>
+class player{
+public:
     int score {501};
     std::vector<uint8_t> darts;
     double legAvg {};
 
-    double calcLegAvg(player p){
-        double legAvg {};
-        for(uint8_t i : p.darts){
-            legAvg = legAvg + i;
+    double calcLegAvg(){
+        if(darts.empty()){
+            return 0.00;
         }
-        legAvg = (legAvg/p.darts.size())*3;
-        return legAvg;
+
+        double legAvg {};
+        for(uint8_t d : darts){
+            legAvg += d;
+        }
+        legAvg = (legAvg/darts.size())*3;
+        return (std::round(legAvg * 100) / 100);
     }
 };
 
