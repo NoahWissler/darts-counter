8ca5cf1c1d67527736c3f6c6547ba1cd73e33d14
Noah Wissler <noah.wissler@gmail.com>
2025-12-25 14:27:01 +0100

    added stylesheet

--- darts-counter/main.cpp
+++ darts-counter/main.cpp
@@ -2,9 +2,17 @@
 
 #include <QApplication>
 
+#include <QFile>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // Set app style sheet
+    QFile styleSheetFile(":/styles/Darkeum.qss");
+    styleSheetFile.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(styleSheetFile.readAll());
+    a.setStyleSheet(styleSheet);
+
     MainWindow w;
     w.show();
     return a.exec();
